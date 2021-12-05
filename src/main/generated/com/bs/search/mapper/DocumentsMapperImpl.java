6bd51ec4d973b26410982b8c0f50724b65e0eef8
yelee <atoto0311@gmail.com>
2021-12-06 04:44:55 +0900

    chore : BookRepositoryTest 케이스 생성 정상작동 확인

---
+++ src/main/generated/com/bs/search/mapper/DocumentsMapperImpl.java
@@ -0,0 +1,39 @@
+package com.bs.search.mapper;
+
+import com.bs.search.domain.y;
+import com.bs.search.domain.BookEntity.BookEntityBuilder;
+import com.bs.search.vo.BookApi.Documents;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-12-06T03:04:22+0900",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
+)
+public class DocumentsMapperImpl implements DocumentsMapper {
+
+    @Override
+    public BookEntity bookApiToEntity(Documents documentsVO, long id) {
+        if ( documentsVO == null ) {
+            return null;
+        }
+
+        BookEntityBuilder bookEntity = BookEntity.builder();
+
+        if ( documentsVO != null ) {
+            bookEntity.title( documentsVO.getTitle() );
+            bookEntity.contents( documentsVO.getContents() );
+            bookEntity.url( documentsVO.getUrl() );
+            bookEntity.isbn( documentsVO.getIsbn() );
+            bookEntity.datetime( documentsVO.getDatetime() );
+            bookEntity.publisher( documentsVO.getPublisher() );
+            bookEntity.price( documentsVO.getPrice() );
+            bookEntity.salePrice( documentsVO.getSalePrice() );
+            bookEntity.thumbnail( documentsVO.getThumbnail() );
+            bookEntity.status( documentsVO.getStatus() );
+        }
+        bookEntity.id( id );
+
+        return bookEntity.build();
+    }
+}
--- src/test/java/com/bs/search/domain/BookRepositoryTest.java
+++ src/test/java/com/bs/search/domain/BookRepositoryTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 /**
  * packageName : com.bs.search.domain
@@ -23,66 +27,52 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * -----------------------------------------------------------
  * 2021-12-05       isbn8         최초 생성
  */
+@Transactional
 @SpringBootTest
 class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
-    private BookEntity bookEntity;
+    void makeData() {
+        //given
+        List<BookEntity> bookList = new ArrayList<>();
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            BookEntity bookEntity = BookEntity.builder()
+                    .id((long) i)
+                    .price((5000L * i))
+                    .title("카카오_" + i)
+                    .build();
+            bookList.add(bookEntity);
+        });
 
-    private static String title = "카카오(역사를 바꾼 물질 이야기 5)";
-
-
-    @BeforeEach
-    void setUp() {
-//        /* given */
-//        List<String> listAuthors = new ArrayList<>();
-//        listAuthors.add("안드레아 더리");
-//        listAuthors.add("토마스 쉬퍼");
-//
-//        List<String> listTrans = new ArrayList<>();
-//        listTrans.add("조규희");
-//        listTrans.add("조규희2");
-        bookEntity = BookEntity.builder()
-                .id(1L)
-                .contents("초콜릿의 달콤함뿐 아니라, 그 이면에 숨겨진 쌉싸래한 속살을 들춰보는『카카오』. 대중에게 사랑받는 기호식품 초콜릿과 그 원재료인 카카오 사이의 간극을 역사·인문·사회학적 관점에서 다루고 있다. 크게 식물 자체로서의 카카오와 재배 방법/ 카카오 재배농민의 현실과 공정무역/ 카카오의 기원과 메소아메리카 역사/ 유럽 초콜릿 문화사로 나뉜다. 시리즈의 특성답게 한 주제를 다각도로 조명했다는 점, 간결하고 편안한 글투로 흥미를 이끌어 낸다.")
-                .datetime("2014-08-11T00:00:00.000+09:00")
-                .isbn("8997429426 9788997429424")
-                .price(22000)
-                .publisher("자연과생태")
-                .salePrice(19800)
-                .status("정상판매")
-                .thumbnail("https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1487445%3Ftimestamp%3D20211119163444")
-                .title("카카오(역사를 바꾼 물질 이야기 5)")
-                .build();
+        //when
+        bookRepository.saveAll(bookList);
     }
 
     @Test
     @DisplayName("조회된 도서 정보 저장 확인 테스트")
     void saveAll(){
-        // given
-        List<BookEntity> bookList = new ArrayList<>();
-        bookList.add(bookEntity);
+        //given, when
+        makeData();
 
-        // when
-        bookRepository.saveAll(bookList);
-
-        // then
-        assertThat(bookRepository.findById(bookEntity.getId()))
-                .isNotEmpty();
-
+        //then
+        assertAll(
+                () -> assertThat(bookRepository.findById(1L)).isNotEmpty(),
+                () -> assertThat(bookRepository.findById(5L)).isNotEmpty(),
+                () -> assertThat(bookRepository.findById(10L)).isNotEmpty()
+        );
 
     }
 
-    //도서정보 저장시 id가 널이면 안됨
     @Test
     @DisplayName("도서정보 저장시 ID는 필수 정보")
     void saveWithoutId() {
         // given
-        Long id = 111L;
+        Long id = null;
 
         // when
         BookEntity book = BookEntity.builder()
+                                .id(id)
                                 .isbn("test isbn")
                                 .title("kakao")
                                 .build();
